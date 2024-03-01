a12e143b9f837cdea150256c870dd9a810f84532
lumynou5 <lumynou5.tw@gmail.com>
2024-03-01 18:02:37 +0800

    Implement removal

--- queue.c
+++ queue.c
@@ -65,13 +65,24 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+
+    element_t *element = list_first_entry(head, element_t, list);
+    list_del(&element->list);
+    if (sp && element->value) {
+        strncpy(sp, element->value, bufsize);
+        sp[bufsize - 1] = '\0';
+    }
+    return element;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+    return q_remove_head(head->prev->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
